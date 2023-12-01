7299f0886c46748e4992ff8587035bc09b41f057
abimael-libaino <abimael.libaino@gmail.com>
2023-11-30 22:07:51 -0300

    Retirando o arquivo .md vazio e colocando a primeira versão do codigo

---
+++ desafio1.js
@@ -0,0 +1,20 @@
+var xp = 3000
+var nome = "Abi"
+var nivel = "     "
+if(xp<1001){
+ nivel = "Ferro"
+} else if(xp>1000 && xp<2001){
+    nivel = "Bronze"
+} else if(xp>2000 && xp<5001){
+    nivel = "Prata"
+} else if(xp>6000 && xp<7001){
+    nivel = "Ouro"
+} else if(xp>7000 && xp<8001){
+    nivel = "Platina"
+} else if(xp>8000 && xp<9001){
+    nivel = "Ascendente"
+} else if(xp>9000 && xp<10001){
+    nivel = "Imortal"
+} else if(xp>10000){
+    nivel = "Radiante"}
+console.log("O Héroi de nome " + nome + " está no nível de " + nivel)
